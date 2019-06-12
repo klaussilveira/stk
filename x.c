8df456b3a1ab79199a1bde8c042d40c85c59e25b
Klaus Silveira <contact@klaussilveira.com>
2019-06-11 23:54:41 -0400

    Applied clipboard patch from https://st.suckless.org/patches/clipboard

--- x.c
+++ x.c
@@ -645,6 +645,8 @@ setsel(char *str, Time t)
 	XSetSelectionOwner(xw.dpy, XA_PRIMARY, xw.win, t);
 	if (XGetSelectionOwner(xw.dpy, XA_PRIMARY) != xw.win)
 		selclear();
+
+	clipcopy(NULL);
 }
 
 void
@@ -662,7 +664,7 @@ brelease(XEvent *e)
 	}
 
 	if (e->xbutton.button == Button2)
-		selpaste(NULL);
+		clippaste(NULL);
 	else if (e->xbutton.button == Button1)
 		mousesel(e, 1);
 }
